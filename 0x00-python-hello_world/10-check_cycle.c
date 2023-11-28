65cb1d7b0daabbac5e0fd73f29b80c058059d6f0
Bane <marcytornad@gmail.com>
2023-11-27 19:15:03 -0800

    ten

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,16 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *fast = list;
 
 	if (list == NULL || list->next == NULL)
 		return (0); /* single node or empty list == no cycle */
 	while (fast != NULL && fast->next != NULL)
 	{
-		if (slow == fast)
-			return (1); /* Cycle detected */
 		slow = slow->next;
 		fast = fast->next->next;
+		if (slow == fast)
+			return (1); /* Cycle detected */
 	}
 	return (0); /* No cycle found */
 }
