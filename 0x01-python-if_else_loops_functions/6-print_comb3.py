30b4ae961cbef9198587118b07b182f30044c45c
Bane <marcytornad@gmail.com>
2023-11-28 06:06:10 -0800

    low

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(0, 100):
+    if i != 89:
+        if i < 10 or str(i)[0] < str(i)[1]:
+            print("{:02}".format(i), end=", ")
+    else:
+        print("{}".format(i))
