c7194b46f75547a1df5868b0dee3dd7b3fd0d0f3
Bane <marcytornad@gmail.com>
2023-11-30 03:25:59 -0800

    3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    add = add_0.add
     print("{} + {} = {}".format(a, b, add(a,b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("{} argument:".format(argc))
+        print("1: {}".format(argv[2]))
+    elif argc > 1:
+        print("{} arguments:".format(argc))
+        for count, arg in enumerate(argv[1:], start=1):
+              print("{}: {}".format(count, arg))
