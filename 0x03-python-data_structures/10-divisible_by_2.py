859775728b7c30c89baf35196ea58dbee2bef8c5
Bane <marcytornad@gmail.com>
2023-12-02 15:30:59 -0800

    9

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list == []:
+        return None
+    check = my_list[:]
+    for index, i in enumerate(my_list):
+        if i % 2 == 0:
+            check[index] = True
+        else:
+            check[index] = False
+    return check
