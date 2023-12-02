1fe266a364033b8dc6207b74d562a744d9686710
Bane <marcytornad@gmail.com>
2023-12-02 15:07:17 -0800

    3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix != [[]]:
+        for row in matrix:
+            for index, i in enumerate(row):
+                if index == len(row) - 1:
+                    print("{}".format(i))
+                else:
+                    print("{}".format(i), end=' ')
+    else:
+        print("{}".format("\n"), end="")
