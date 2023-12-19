859d0e8e57a6102a77902e5c25c8790a7efb06ad
Bane <marcytornad@gmail.com>
2023-12-19 15:46:21 -0800

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,13 +6,15 @@ class Square:
     """A class Square that defines a square
     Atributes:
         __size (int): size of the square
+        __position (tuple): position of num in square
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """initializes each object
         args:
             size (int): size of the square
         """
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -35,6 +37,26 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """"getter method
+        Return:
+        size as RO
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """sets square to position
+        args:
+            value (tuple): pos in the square
+        """
+        if (not isinstance(value, tuple)) or (len(value) != 2) or \
+           (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     def area(self):
         """finds square area from size
         return:
@@ -47,5 +69,7 @@ class Square:
         if not self.__size:
             print()
         else:
-            for _ in (self.__size):
-                print("#" * self.__size)
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
