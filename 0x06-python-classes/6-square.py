e855f03df4dd9d8353964c09b223d222af1c6849
Bane <marcytornad@gmail.com>
2023-12-19 18:07:08 -0800

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,13 +24,16 @@ class Square():
 
     @size.setter
     def size(self, value):
-        if type(value) is int:
-            if value >= 0:
-                self.__size = value
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+        # """sets value of size
+        # args:
+        #     value (int): size of the square
+        # """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
