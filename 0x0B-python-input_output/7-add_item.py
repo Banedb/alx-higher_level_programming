8d29c74588a755bf8754e55e34ef637b46f687ad
Bane <marcytornad@gmail.com>
2024-01-29 08:43:36 -0800

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,12 +8,13 @@ from sys import argv
 from os.path import exists
 
 
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
-f = "add_item.json"
-if exists(f):
-    my_list = load_from_json_file(f)
-else:
-    my_list = []
-my_list.extend(argv[1:])
-save_to_json_file(my_list, f)
+if __name__ == "__main__":
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
+    f = "add_item.json"
+    if exists(f):
+        my_list = load_from_json_file(f)
+    else:
+        my_list = []
+        my_list.extend(argv[1:])
+        save_to_json_file(my_list, f)
