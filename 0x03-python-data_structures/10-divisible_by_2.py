466fa1c84c7c748b8b25a9e4b16125cd452ccf29
Bane <121007715+Banedb@users.noreply.github.com>
2025-03-18 16:52:22 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,5 @@
 def divisible_by_2(my_list=[]):
     if my_list == []:
         return None
-    check = my_list[:]
-    for index, i in enumerate(my_list):
-        if i % 2 == 0:
-            check[index] = True
-        else:
-            check[index] = False
-    return check
+    new = [i % 2 == 0 for i in my_list]
+    return new
