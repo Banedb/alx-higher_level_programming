63c8afc6de89d068db06b357f405ba313cc2eac1
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-03 15:45:59 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,7 +14,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-
-    #  def area(self):
-        #  """"returns the area of the square"""
-        #  return self.__size ** 2
