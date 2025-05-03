02b8e5696f5956ab43cf7da4d09d24ae7b6641bf
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-03 18:33:25 +0100

    Fix 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-""" add_attribute module """
+"""101-add_attribute module"""
 
 
-def add_attribute(prmObject, prmName, prmValue):
-    """ add_attribute function """
-    if not hasattr(prmObject, "__dict__"):
+def can_assign_attr(obj, attr):
+    """Checks if obj can be assigned an attribute"""
+    if not hasattr(obj, "__dict__"):
+        return False
+
+    cls = type(obj)
+    slots = getattr(cls, "__slots__", None)
+
+    if not slots:  # Any attribute can be set
+        return True
+
+    if isinstance(slots, str):  # Check if slots is str
+        return slots == attr
+
+    return attr in slots  # check if attr is in slots list/tuple
+
+
+def add_attribute(obj, name, value):
+    if can_assign_attr(obj, value):
+        setattr(obj, name, value)
+    else:
         raise TypeError("can't add new attribute")
-    if (not hasattr(prmObject, prmName)):
-        prmObject.__setattr__(prmName, prmValue)
