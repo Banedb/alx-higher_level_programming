77c616b5ac533b212d9dd69222e50a20b00dff7f
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-03 17:24:25 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
-Contains the class MyInt
+100-my_int module
 """
 
 
 class MyInt(int):
-    """rebel version of an integer, perfect for opposite day!"""
-    def __new__(cls, *args, **kwargs):
-        """create a new instance of the class"""
-        return super(MyInt, cls).__new__(cls, *args, **kwargs)
-
+    """Reverse implementation of != and == operators"""
     def __eq__(self, other):
         """what was != is now =="""
         return int(self) != other
