54898fdb2986b5b57447c815bb192815508e5265
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-04 21:46:55 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+"""Module that defines the class Student."""
 
 
 class Student:
-    """ Class to create student instances """
+    """Class to create student instances."""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initialises an instance of Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Method that returns directory description """
-        obj = self.__dict__.copy()
-        if type(attrs) is list:
-
-            for item in attrs:
-                if type(item) is not str:
-                    return obj
-
-            d_list = {}
-
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-
-        return obj
+        """Retrieves a dictionary representation of a Student instance."""
+        if type(attrs) is list and all(type(attr) is str for attr in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
