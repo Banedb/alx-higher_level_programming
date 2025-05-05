07f6509a860a4c15b06651251e3234a6232e7d54
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-05 16:30:34 +0100

    Fix 101-stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,27 +5,28 @@ and prints after every 10 lines."""
 
 import sys
 
-total = 0
+file_size = 0
 status_table = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
 
 
 def print_stats():
     """Prints stats"""
-    print(f"File size: {total}")
-    for i in sorted(status_table):
-        if status_table[i] > 0:
-            print(f"{i}: {status_table[i]}")
+    print(f"File size: {file_size}")
+    for k, v in sorted(status_table.items()):
+        if v > 0:
+            print(f"{k}: {v}")
 
 
 try:
     for count, line in enumerate(sys.stdin, start=1):
         my_list = line.split()
-        total += int(my_list[-1])
+        file_size += int(my_list[-1])
         status_table[my_list[-2]] += 1
 
         if count % 10 == 0:
             print_stats()
+    print_stats()
 except KeyboardInterrupt:
     print_stats()
     raise
