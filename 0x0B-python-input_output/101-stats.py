f57117de0f405e1fea4cd0bb0ab56e3c396aae87
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-05 16:12:54 +0100

    check 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,58 +1,31 @@
 #!/usr/bin/python3
-"""101-stats
-    Reads from standard input and computes metrics
-"""
+"""101-stats module: Reads stdin line by line, computes metrics
+and prints after every 10 lines."""
+
 
 import sys
 
+total = 0
+status_table = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+
 
 def print_stats():
-    """Prints metrics
-    """
-    print('File size: {:d}'.format(file_size))
+    """Prints stats"""
+    print(f"File size: {total}")
+    for i in sorted(status_table):
+        if status_table[i] > 0:
+            print(f"{i}: {status_table[i]}")
 
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
-
-
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
-lc = 0
-file_size = 0
 
 try:
-    for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
+    for count, line in enumerate(sys.stdin, start=1):
+        my_list = line.split()
+        total += int(my_list[-1])
+        status_table[my_list[-2]] += 1
+
+        if count % 10 == 0:
             print_stats()
-
-        pieces = line.split()
-
-        try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except (IndexError, ValueError) as e:
-            pass
-
-        try:
-            file_size += int(pieces[-1])
-        except (IndexError, ValueError) as e:
-            pass
-
-        lc += 1
-
-    print_stats()
 except KeyboardInterrupt:
     print_stats()
     raise
