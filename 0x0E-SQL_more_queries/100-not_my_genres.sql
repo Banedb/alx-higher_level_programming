4188de9fa7bae65e38ab1b06106fb9f67694a3ac
Bane <121007715+Banedb@users.noreply.github.com>
2025-06-20 12:09:34 +0100

    Add files via upload

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,10 @@
--- Lists all genres not linked to the show Dexter
-SELECT tv_genres.name
-FROM tv_genres
-WHERE tv_genres.id NOT IN
-(SELECT tv_genres.id
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter")
+-- List all genres not linked to the show `Dexter` in the database `hbtn_0d_tvshows`.
+SELECT tv_genres.name FROM tv_genres
+WHERE NOT EXISTS
+(
+	SELECT 1 FROM tv_show_genres
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_show_genres.genre_id = tv_genres.id
+	AND tv_shows.title = 'Dexter'
+)
 ORDER BY tv_genres.name;
