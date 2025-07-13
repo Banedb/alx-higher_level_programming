936bae2d219ec43bd800900743727b0a7eb7494c
Bane <121007715+Banedb@users.noreply.github.com>
2025-07-13 14:51:56 +0100

    0 & 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Prints 3 lines.
-const myvar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myvar);
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
