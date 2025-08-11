6a7c1d2e2693a406e1da02ea4ad04692a8640594
Bane <marcytornad@gmail.com>
2025-08-11 14:25:34 -0700

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a request and displays size of body of response.
-curl -sLX PUT -d "user_id=98" -H "Origin: ALX" 0.0.0.0:5000/catch_me
-
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
