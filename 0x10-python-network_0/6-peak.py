6fe08e412422edff5ff1f1d70e696f6f39cb351a
Bane <marcytornad@gmail.com>
2025-08-11 15:44:56 -0700

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,10 +8,10 @@ def find_peak(list_of_integers):
         return
     if list_of_integers[0] > list_of_integers[1]:
         return list_of_integers[0]
+    if list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
     for idx, i in enumerate(list_of_integers[1:-1], start=1):
         if i > list_of_integers[idx - 1] and i > list_of_integers[idx + 1]:
             return i
     if list_of_integers[0] == list_of_integers[1]:
         return list_of_integers[0]
-    if list_of_integers[-1] > list_of_integers[-2]:
-        return list_of_integers[-1]
