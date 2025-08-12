807bfbd6316dafc4027c73ef9a8f174c88798fbf
Bane <marcytornad@gmail.com>
2025-08-12 09:49:22 -0700

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
     content = r.text
     print("Body response:")
-    print(f"\t - type: {type(content)}")
-    print(f"\t - content: {content}")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
